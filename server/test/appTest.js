b42d64a097bee97e59bcad85ee03d371a6879fee
Bohdan Kovtun <kirpich1337228@gmail.com>
2021-04-26 15:15:22 +0300

    async test

--- server/test/appTest.js
+++ server/test/appTest.js
@@ -1,35 +1,35 @@
 const assert = require('chai').assert;
-const {Game} = require('../src/game')
+const { Game } = require('../src/game')
 const { EventEmitter } = require('events');
-describe('App', function(){
-    it('direction test', function(){
-    
+describe('App', function () {
+    it('take Zerno Test', function (done) {
+        this.timeout(300);
         function insert(index, item) {
             this.splice(index, 0, item);
         }
         Array.prototype.insert = insert;
-
+        var answer=0;
         let emitter = new EventEmitter();
-        emitter.on('snakeTakeZernoCommand', (indx, game) =>{
+        emitter.on('snakeTakeZernoCommand', (indx, game) => {
             game.stop();
-            if (indx != 0){
+            if (indx != 0) {
                 throw 'Error';
             }
+            answer=1;
         })
         let game = new Game(30, 30, emitter);
-        game.zerno.pos.x=10;
-        game.zerno.pos.y=5;
+        game.zerno.pos.x = 10;
+        game.zerno.pos.y = 5;
         game.createSnake();
         game.start(10);
+        setTimeout(function () {
+            if (answer == 1){
+                done();
+            
+            }
+            
+        }, 100);
 
-       
     })
 
-    it('direction test', function(){
-    
-       assert("w", "w")
-
-       
-    })
-    
 })
